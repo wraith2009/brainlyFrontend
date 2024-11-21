76662d0cb5972657652e37da58365b0ff3c6187a
Rahul Bhardwaj <113882096+wraith2009@users.noreply.github.com>
2024-11-21 22:17:55 +0530

    feat: add @vercel/analytics for improved performance tracking and update script source in index.html

--- src/main.tsx
+++ src/main.tsx
@@ -3,10 +3,12 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
 import { RecoilRoot } from "recoil";
+import { Analytics } from "@vercel/analytics/react";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <RecoilRoot>
+      <Analytics />
       <App />
     </RecoilRoot>
   </StrictMode>
