d721980bc0f03932ed2618e0b74cb075ded60a02
Rahul Bhardwaj <113882096+wraith2009@users.noreply.github.com>
2024-11-23 16:18:58 +0530

    fix: update API_URL to point to production server; update script source in index.html

--- src/config/api.ts
+++ src/config/api.ts
@@ -1,2 +1,3 @@
 export const API_URL =
-  import.meta.env.REACT_APP_API_URL || "http://localhost:3000/api/v1";
+  import.meta.env.REACT_APP_API_URL ||
+  "https://brainly-frontend-sable.vercel.app/api/v1";
